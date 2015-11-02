de5280e96ed40128fe9e11b1a77603d7cadb9c19
Patrick Lai <patrickcylai@gmail.com>
2015-11-02 17:50:42 +1100

    Added the recording of Red LED data
    the matlab program is now able to record the data from the Red LED and
    I have also added proper messages to the command window.

--- pulse_oximeter.m
+++ pulse_oximeter.m
@@ -1,24 +1,55 @@
-clc;
-clear;
 close all;
+disp('Setting up configuration...');
 a=arduino();
 Sensorpin = 'A0';
 IRpin = 'D2';
 REDpin = 'D3';
 
-IRon = false;
-REDon = true;
+%% IR LED
 writeDigitalPin(a, IRpin,1);
 pause(2);
 interv = 1000;
 t=1;
-x=0;
+ir=0;
+disp('Recording IR Data');
+disp('Please wait...')
+tic;
 while t<interv
    voltage =  readVoltage(a, Sensorpin);
-   x=[x,voltage];
-   %drawnow;
+   ir=[ir,voltage];
+%    plot(x)
+%    drawnow;
    t=t+1;
 end
-
-plot(x);
+elapsed_IR = toc
+disp('Completed IR');
+time_IR = linspace(0,elapsed_IR,1000);
+plot(time_IR,ir);
 grid on;
+xlabel('Time (s)'); ylabel('Voltage'); title('Pulse Oximeter IR Data');
+
+%% RED LED
+writeDigitalPin(a, REDpin,1);
+writeDigitalPin(a, IRpin,0);
+pause(2);
+t=1;
+red=0;
+disp('Recording RED Data');
+disp('Please wait...')
+tic;
+while t<interv
+   voltage =  readVoltage(a, Sensorpin);
+   red=[red,voltage];
+%    plot(x)
+%    drawnow;
+   t=t+1;
+end
+elapsed_RED = toc
+disp('Completed RED');
+time_RED = linspace(0,elapsed_RED,1000);
+
+figure;
+plot(time_RED,red);
+grid on;
+xlabel('Time (s)'); ylabel('Voltage'); title('Pulse Oximeter RED Data');
+clear a;
